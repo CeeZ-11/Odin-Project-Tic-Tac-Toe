bda3420687a3362877a5612c5aec206ea61af7a4
Seamor Estrabon <seamorestrabon@gmail.com>
2024-08-29 12:55:08 -0700

    Update module name to gameBoard
    Adds createGameBoard function
    
    Update dialog; adds addPlayer function 'unfinished'

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var game = (function () {
+var gameBoard = (function () {
+  const player = {};
+
   const dialog = document.querySelector("dialog");
-  const gameInitiate = document.getElementById("startGame"); // Button with id "Submit"
-  const playerOne = document.getElementById("player1");
-  const playerTwo = document.getElementById("player2");
+  const gameInitiate = document.getElementById("startGame");
 
   const closeModal = () => {
     dialog.close();
@@ -11,5 +11,32 @@ var game = (function () {
   gameInitiate.addEventListener("click", () => {
     dialog.classList.add("inactive");
     closeModal();
+    addPlayer();
+    createGameBoard();
   });
+
+  const addPlayer = () => {
+    const playerOne = document.getElementById("player-one");
+    const playerTwo = document.getElementById("player-two");
+    player.name = playerOne.value;
+    player.symbol = "X";
+    console.log(playerOne.value);
+    console.log(playerTwo.value);
+    console.log(player.name);
+    console.log(player);
+  };
+
+  const createGameBoard = () => {
+    const board = document.getElementById("game-board");
+
+    for (let i = 0; i < 9; i++) {
+      const cell = document.createElement("div");
+      const cellButton = document.createElement("button");
+      cell.classList.add("cell");
+      cellButton.classList.add("cell-button");
+      cell.id = `cell-${i + 1}`;
+      board.appendChild(cell);
+      cell.appendChild(cellButton);
+    }
+  };
 })();
